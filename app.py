38916164d124ff7afcc29105401b431ca61e94ca
apmoskal <moskalap@bc.edu>
2025-11-18 18:31:48 -0500

    Refactor FastAPI app and update prediction logic
    Refactor the FastAPI app by removing unused imports and comments. Adjust the predict function to create a DataFrame for predictions.

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-from fastapi import FastAPI, Form
-from fastapi.responses import HTMLResponse
-import xgboost as xgb
-import numpy as np
-from pathlib import Path
-import pandas as pd
-
-app = FastAPI()
-
-# Load XGBoost model
-model = xgb.Booster()
-model.load_model("model/model.json")
-
-BASE_DIR = Path(__file__).resolve().parent  # directory of app.py
-HTML_FILE = BASE_DIR / "frontend" / "index.html"
-
-# @app.get("/", response_class=HTMLResponse)
-# def read_root():
-#     try:
-#         content = HTML_FILE.read_text()
-#         return HTMLResponse(content=content)
-#     except Exception as e:
-#         return HTMLResponse(content=f"<h1>Error loading HTML</h1><p>{e}</p>")
-
-@app.get("/", response_class=HTMLResponse)
-def read_root():
-    try:
-        content = HTML_FILE.read_text()
-        return HTMLResponse(content=content)
-    except Exception as e:
-        return HTMLResponse(content=f"<h1>Error loading HTML</h1><p>{e}</p>")
-
-
 @app.post("/predict")
 def predict(
     lag1: float = Form(...),
@@ -40,7 +7,6 @@ def predict(
     lag5: float = Form(...),
     lag6: float = Form(...)
 ):
-     # Create a DataFrame with the same column names as used in training
     data = pd.DataFrame([{
         "Sales_Lag_1_Month": lag1,
         "Sales_Lag_2_Month": lag2,
@@ -52,7 +18,6 @@ def predict(
     dmatrix = xgb.DMatrix(data)
     prediction = model.predict(dmatrix)[0]
     return {"prediction": float(prediction)}
-    
     data = np.array(input.sales).reshape(1, -1)
     prediction = model.predict(data)[0]
     return {"prediction": float(prediction)}
